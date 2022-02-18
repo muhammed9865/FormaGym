badf808ece0289a78e6f887a0c99672b3765c77f
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-02-18 13:45:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/formagym/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.formagym.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.formagym.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
